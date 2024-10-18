4f86351989e9df5000d22c54678639d4d70664fa
Fihdi <35708046+Fihdi@users.noreply.github.com>
2024-10-18 09:16:25 +0200

    Update Bernoulli-Gates.ino

--- Euclid/Bernoulli-Gates.ino
+++ Euclid/Bernoulli-Gates.ino
@@ -1,10 +1,10 @@
 //Send gates or triggers into the CLK IN input.
 //The gate (or trigger) will randomly be routed to either OUT1 or OUT2 depending on a probability.
-//This probability is set by the potentiometer for the amount of steps at the first Euclid Generator.
+//This probability is set by the rate potentiometer
 
 //Inputs
 #define CLK 4         //External Clock Pin
-#define S1_IN A3  //Voltage for Amount of steps of the first Euclid Rhythm
+#define CLKI_RATE A0  //Voltage for Amount of steps of the first Euclid Rhythm
 
 //Outputs
 #define CLKO 2  //Internal Clock Output
@@ -20,9 +20,8 @@ volatile bool CLKtriggered = false;
 void setup() {
 
   //No INPUT_PULLUP needed because of the external 10k resistors.
-  Serial.begin(9600);
   pinMode(CLK, INPUT_PULLUP);
-  pinMode(S1_IN, INPUT);
+  pinMode(CLKI_RATE, INPUT);
 
   pinMode(OUT1, OUTPUT);
   pinMode(OUT2, OUTPUT);
@@ -33,7 +32,6 @@ void setup() {
 void loop() {
 
   checkClock();
-  //Inputs have pullup resistors instead of pulldown, thus the trigger conditioning is inverted from e.g.: my Sequencer.
 }
 
 void checkClock() {
@@ -59,14 +57,12 @@ writeOutputs();
   }
 }
 
-
 void writeOutputs() {
 //New refom number
   ref = random(1024);
-  Serial.println(ref);
 //Output Bernoulli Gates
 
-if(ref > analogRead(S1_IN)){
+if(ref > analogRead(CLKI_RATE)){
   digitalWrite(OUT1, HIGH);
   digitalWrite(OUT2, LOW);
 }else{
