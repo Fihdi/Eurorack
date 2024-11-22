ce6c6398b2bc7258d2faceea3e2b9de082978ac7
Cedric Stoquer <cstoquer@gmail.com>
2024-11-22 18:24:14 +0900

    add TestHardware firmware

---
+++ SimpleSampler/firmwares/TestHardware/README.md
@@ -0,0 +1,14 @@
+# Test Hardware
+
+Test hardware UI.
+
+## Interface
+
+| UI          | function
+| ----------- | --------------------
+| Trigger     | Make the LED Flash momentarily
+| Encoder     | *not used*
+| Speed       | Controls LED brightness
+| Random      | *not used*
+| Part/Entire | *not used*
+| LED         | Test feedback
---
+++ SimpleSampler/firmwares/TestHardware/TestHardware.cpp
@@ -0,0 +1,49 @@
+#include "daisy_seed.h"
+#include "../PTAL/hardwares/Hardware.h"
+
+
+using namespace daisy;
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+Hardware hardware;
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+static void AudioCallback (
+  daisy::AudioHandle::InputBuffer  in,
+  daisy::AudioHandle::OutputBuffer out,
+  size_t size) {
+
+  // Clear buffer
+  memset(out[0], 0, size * sizeof(float));
+  memset(out[1], 0, size * sizeof(float));
+}
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+int main (void) {
+  // initializations
+  hardware.Init();
+
+  //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  // Setup audio callback
+  hardware.daisy.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+  hardware.daisy.SetAudioBlockSize(32);
+  hardware.daisy.StartAudio(AudioCallback);
+
+
+  //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  while (true) {
+    hardware.Update();
+
+    if (hardware.cvInput.changed) {
+      hardware.led.SetPwm(hardware.cvInput.value);
+    }
+
+    if (hardware.gatePlay->risingEdge) {
+      hardware.led.SetFlash();
+    }
+
+    // TODO: test encoder
+  }
+}
