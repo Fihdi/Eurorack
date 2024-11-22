880e3aaeeec520f51436f8a7ba413666512f659b
Cedric Stoquer <cstoquer@gmail.com>
2024-11-22 18:23:16 +0900

    add Blink firmware

---
+++ SimpleSampler/firmwares/Blink/Blink.cpp
@@ -0,0 +1,42 @@
+#include "daisy_seed.h"
+#include "../PTAL/hardwares/Hardware.h"
+
+
+using namespace daisy;
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+Hardware hardware;
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+static void AudioCallback (
+  daisy::AudioHandle::InputBuffer  in,
+  daisy::AudioHandle::OutputBuffer out,
+  size_t size) {
+
+  // Clear buffer
+  memset(out[0], 0, size * sizeof(float));
+  memset(out[1], 0, size * sizeof(float));
+}
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+int main (void) {
+  // initializations
+  hardware.Init();
+
+  //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  // Setup audio callback
+  hardware.daisy.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+  hardware.daisy.SetAudioBlockSize(32);
+  hardware.daisy.StartAudio(AudioCallback);
+
+
+  //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  while (true) {
+    hardware.led.SetSolid(true);
+	System::Delay(200);
+	hardware.led.SetSolid(false);
+	System::Delay(200);
+  }
+}
